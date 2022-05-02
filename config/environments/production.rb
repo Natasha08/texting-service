a3cec5563e54d4285c07833bcbd63f7f2db7832b
Natasha <admin@natashaosborne.com>
2022-05-02 11:13:47 -0600

    allow any host?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
   config.web_socket_server_url = "wss://#{ENV['DOMAIN']}/cable"
+  config.hosts = nil
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
